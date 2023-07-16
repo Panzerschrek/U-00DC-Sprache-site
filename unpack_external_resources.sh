55ea1e55d58795c52f4c8b03bc150bab60a4e9c0
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-07-16 17:47:19 +0300

    Popravlen putj k direktorii.

--- unpack_external_resources.sh
+++ unpack_external_resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv build/Interpreter.js Interpreter.js &&\
-mv build/Interpreter.wasm Interpreter.wasm &&\
-mv build/Interpreter_launcher.js Interpreter_launcher.js &&\
-rm -r build
+mv "Interpreter build/Interpreter.js" "Interpreter.js" &&\
+mv "Interpreter build/Interpreter.wasm" "Interpreter.wasm" &&\
+mv "Interpreter build/Interpreter_launcher.js" "Interpreter_launcher.js" &&\
+rm -r "Interpreter build"
