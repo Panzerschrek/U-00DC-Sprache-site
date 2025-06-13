f85f1d90db7ce3a46d465c077f9f6d4799afc85d
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-06-13 20:07:33 +0300

    Kacajem i raspakovyvajem rasširenije dlä "Visual Studio".

--- unpack_external_resources.sh
+++ unpack_external_resources.sh
@@ -3,4 +3,6 @@
 mv "Interpreter build/Interpreter.js" "Interpreter.js" &&\
 mv "Interpreter build/Interpreter.wasm" "Interpreter.wasm" &&\
 mv "Interpreter build/Interpreter_launcher.js" "Interpreter_launcher.js" &&\
-rm -r "Interpreter build"
+rm -r "Interpreter build" &&\
+mv "VisualStudioExtension/Ü_extension.vsix" "Ü_extension.vsix" &&\
+rm -r "VisualStudioExtension"
